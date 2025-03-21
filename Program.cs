dce897e1819f8dad478b24101822089715e2e8ee
Lucas Pinheiro <98431467+pucaslinheiro@users.noreply.github.com>
2025-03-21 18:30:45 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO.Pipelines;
+
+namespace AULA_03___Variáveis
+{
+    class Program 
+    {
+        static void Main (string [] args) 
+        {
+            Console.Write("Qual o resultado desta equação: 7 * 32 - 77?\n");
+            
+            int answer01 = Convert.ToInt32 ( Console.ReadLine ());
+
+            if (answer01 == 147) { 
+                Console.Write ("\nParabéns, você acertou!");
+            } else {
+                Console.Write ("\nVocê errou!");
+            }
+
+            Console.Write("\nQual o resultado desta equação: 48 + 61 / 7?\n");
+            
+            int answer02 = Convert.ToInt32(Console.ReadLine());
+
+            if (answer02 == 57) {
+                Console.Write ("\nParabéns, você acertou!");
+            } else {
+                Console.Write ("\nVocê errou!");
+            }
+
+            // Pra esperar alguma tecla do usuário.
+            Console.WriteLine("Pressione qualquer tecla para sair...");
+            Console.ReadKey();
+        }
+    }
+}
